af9b1e2ec97fc1e357195ca6101d53711ab93c09
Mukesh Chevula <145662081+mukesh-chevula@users.noreply.github.com>
2025-05-17 20:48:59 +0530

    Update cartActions.js

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -7,7 +7,7 @@ import {
 } from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
-  const { data } = await axios.get(`http://localhost:8000/api/products/${id}`);
+  const { data } = await axios.get(`https://indiashop-y7ma.onrender.com/api/products/${id}`);
 
   dispatch({
     type: CART_ADD_ITEM,
